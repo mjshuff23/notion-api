b62be29fedbf83d03e36cf096bfa664e4c29431d
Michael Shuff <mjshuff23@gmail.com>
2022-01-27 11:36:02 -0500

    testing

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-const { Client } = require('@notionhq/client');
-
-require('dotenv').config();
+import { Client } from '@notionhq/client';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
-(async () => {
-  const listUsersResponse = await notion.users.list({});
-  //   console.log(listUsersResponse);
-})();
+const listUsersResponse = await notion.users.list({});
+//   console.log(listUsersResponse);
 
-(async () => {
-  const myPage = await notion.databases.query({
-    database_id: 'ccdcdced5f774baaac57f2bfdffa296e',
-  });
-  console.log(myPage);
-})();
+const myPage = await notion.databases.query({
+  database_id: 'ccdcdced5f774baaac57f2bfdffa296e',
+});
+console.log(myPage.results[0].properties.Name.title);
